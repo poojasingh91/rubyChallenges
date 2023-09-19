c552126a27f79eccc73e3b56669603b5935430e9
poojasingh <hi4palspoja@gmail.com>
2023-09-19 13:14:57 +0545

    Committing first page on Ruby on Rails

--- introDuction/first_ruby_test/ruby_fundamentals_challenges-master/ordinal/lib/ordinal.rb
+++ introDuction/first_ruby_test/ruby_fundamentals_challenges-master/ordinal/lib/ordinal.rb
@@ -1,3 +1,11 @@
 def ordinal(number)
-  # your code here
+  if(number==1)
+    "#{number}st"
+  elsif(number==2)
+    "#{number}nd"
+  elsif(number==3)
+    "#{number}rd"
+  else
+    "#{number}th"
+  end
 end
--- introDuction/first_ruby_test/ruby_fundamentals_challenges-master/temperature/lib/temperature.rb
+++ introDuction/first_ruby_test/ruby_fundamentals_challenges-master/temperature/lib/temperature.rb
@@ -1,1 +1,10 @@
 # write your code here
+
+def ftoc(temp)
+    return (5.0/9)*(temp-32)
+end
+
+def ctof(temp)
+    #In ruby, last line it returns the value
+    (9.0/5)*temp+32
+end
